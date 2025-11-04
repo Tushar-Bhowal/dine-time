2cdca750d0a48f74ce2920a6b924da17544842a1
Tushar <bhowaltusharwork21@gmail.com>
2025-11-04 14:18:08 +0530

    Refactor Firebase configuration to use environment variables

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -7,12 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfWepkJJtuuIQnI894BqsHwrQryl8tcJU",
-  authDomain: "dine-time-ec8f6.firebaseapp.com",
-  projectId: "dine-time-ec8f6",
-  storageBucket: "dine-time-ec8f6.firebasestorage.app",
-  messagingSenderId: "862020402084",
-  appId: "1:862020402084:web:ca3be0291e0d0b37915f31"
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
